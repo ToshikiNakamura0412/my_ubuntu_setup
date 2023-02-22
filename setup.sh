84319a3f360195c952eb32edbcaa2daf27c666fa
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-02-23 04:26:01 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -69,6 +69,12 @@ echo "======================="
 echo " vim will be installed"
 echo "======================="
 sudo apt install vim -y
+echo "======================="
+echo " vim will be updated"
+echo "======================="
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt upgrade -y
 
 # neovim
 echo "=========================="
