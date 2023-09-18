444987b68d2e179a70264c791551b1c9b6bcbdba
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-09-19 01:06:05 +0900

    Create installer for act

---
+++ scripts/install_act.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# neovim
+echo "======================="
+echo " act will be installed"
+echo "======================="
+cd ~/
+sudo curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+sudo mv -f ~/bin/act /usr/bin/
+sudo rm -rf ~/bin
+echo ">>> Done"
+echo ""
+
