c041ed40cc32f7183b2a43f754aa48f5a039b8c2
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-10-21 06:27:29 +0900

    Update nvim installer

--- scripts/upgrade_nvim.sh
+++ scripts/upgrade_nvim.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo "========================================"
 echo " neovim will be installed and upgraded"
 echo "========================================"
-wget -O /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-sudo chmod +x /tmp/nvim.appimage
-sudo ln -sf /tmp/nvim.appimage /usr/bin/nvim
+wget -O /opt/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+sudo chmod +x /opt/nvim.appimage
+sudo ln -sf /opt/nvim.appimage /usr/bin/nvim
 sudo apt install xsel -y --no-install-recommends
 echo ">>> Done"
 echo ""
