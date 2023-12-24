4164c466506ae8fbaecc11db6eecb2e5ec19d213
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2023-12-24 20:53:26 +0900

    Update my_setup.sh (#4)

--- scripts/my_setup.sh
+++ scripts/my_setup.sh
@@ -17,6 +17,7 @@ echo ""
 # git user config
 echo -n "setting user config for git... "
 git config --global user.name "Toshiki Nakamura"
-git config --global user.email "ee193014@meiji.ac.jp"
+git config --global user.email "tnakamura.amsl@gmail.com
+"
 echo ">>> Done"
 echo ""
