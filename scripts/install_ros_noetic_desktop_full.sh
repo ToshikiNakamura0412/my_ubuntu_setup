afe08064c8c86a4dfd424d5222fecb74f9ee1806
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-01-07 11:15:54 +0900

    Update installer for noetic (#17)

--- scripts/install_ros_noetic_desktop_full.sh
+++ scripts/install_ros_noetic_desktop_full.sh
@@ -6,15 +6,22 @@ echo "=============================="
 # Setup sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Set up keys
-sudo apt install curl -y
+sudo apt install curl -y --no-install-recommends
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 # Installation
 sudo apt update
-sudo apt install ros-noetic-desktop-full -y
+sudo apt install ros-noetic-desktop -y --no-install-recommends
 # Environment setup
 source /opt/ros/noetic/setup.bash
 # Dependencies for building packages
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-osrf-pycommon python3-catkin-tools -y
+sudo apt install -y --no-install-recommends \
+    python3-rosdep \
+    python3-rosinstall \
+    python3-rosinstall-generator \
+    python3-wstool \
+    python3-osrf-pycommon \
+    python3-catkin-tools \
+    build-essential
 sudo rosdep init
 rosdep update
 # Create workspace
