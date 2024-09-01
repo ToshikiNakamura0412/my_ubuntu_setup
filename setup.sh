6cef24dc29d83ad36ff5ee4141a423ac3ab8e1e2
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-09-01 15:28:22 +0900

    Add install pkgs

--- setup.sh
+++ setup.sh
@@ -31,6 +31,34 @@ function copy_wallpaper() {
     fi
 }
 
+function install_docker() {
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install:
+    sudo apt install -y --no-install-recommends \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin \
+        docker-compose-plugin
+
+    # Add user to docker group
+    sudo gpasswd -a $USER docker
+    sudo chmod 666 /var/run/docker.sock
+}
+
 function install_pkgs() {
     echo "========="
     echo " Install"
@@ -41,7 +69,10 @@ function install_pkgs() {
         gnome-tweaks \
         mozc-utils-gui \
         curl \
-        zsh
+        zsh \
+        htop \
+        simplescreenrecorder
+    install_docker
     echo ">>> Done"
     echo ""
 }
